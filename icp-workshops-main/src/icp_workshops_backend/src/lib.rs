363c04bfd9ecb21154984734c75dba0a84d50fe1
GerBardP <152536016+GerBardP@users.noreply.github.com>
2025-03-20 12:41:51 +0100

    Add files via upload

---
+++ icp-workshops-main/src/icp_workshops_backend/src/lib.rs
@@ -0,0 +1,20 @@
+#[ic_cdk::query]
+fn greet(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[ic_cdk::query]
+fn calculate(a: i32, b: i32, operator: String) -> String {
+    let result = match operator.as_str() {
+        "+" => Some(a+b),
+        "-" => Some(a-b),
+        "*" => Some(a*b),
+        "/" => if b != 0 {Some(a/b)} else {None},
+       _ => None
+    };
+
+    match result {
+        Some(value) => format!("Result: {}", value),
+        None => "Invalid operator oraz division by zero".to_string(),
+    }
+}
